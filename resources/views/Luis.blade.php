e73a4c41e802a2a67c725cddcae6fc6b34cf4991
LuisDlara <luis120391@hotmai.com>
2019-10-02 18:30:58 -0500

    subiendo luis cazador

---
+++ resources/views/Luis.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div id="principal">
+<header>
+			<div id="logo">
+				<h2>Informatica</h2>
+				<p>Aprendiendo con un clic</p>
+			</div>
+		
+			
+				<ul>
+					<li><a href="">Enlace</a></li>
+				</ul>
+			
+		           </header>
+
+	</div>
+
+</body>
+</html
