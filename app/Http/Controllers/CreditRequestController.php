1e756e112df271e002eeae463052286c592a6f5c
urielmonsivais <55606914+urielmonsivais@users.noreply.github.com>
2019-10-24 13:57:54 -0500

    uriel subiendo cambios

---
+++ app/Http/Controllers/CreditRequestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CreditRequest;
+use Illuminate\Http\Request;
+
+class CreditRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CreditRequest  $creditRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CreditRequest $creditRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CreditRequest  $creditRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CreditRequest $creditRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CreditRequest  $creditRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CreditRequest $creditRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CreditRequest  $creditRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CreditRequest $creditRequest)
+    {
+        //
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,25 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('address');
+            $table->string('gender');
+            $table->string('birth_date');
+            $table->string('client_number');
+            $table->string('phone_number');
+            $table->double('total_balance'); // 5000 -> 4500
+
+            // Para las direciones de entrega
+            $table->string('street');
+            $table->string('suburb');
+            $table->string('postal_code');
+            $table->string('state');
+            $table->string('city');
+
+            //  -> abo -> abono
+            $table->longText('credits');
+
+            $table->string('rfc', 50);
+
             $table->rememberToken();
             $table->timestamps();
         });
